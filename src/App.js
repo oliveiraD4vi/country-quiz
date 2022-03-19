6b920b395e8e2ad510a92fa4fa45fec2a2f93f8b
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-03-19 00:51:14 -0300

    [001] css elements added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import TopIcon from './assets/undraw_adventure.svg';
+
+import './styles/global.css';
+
 export default function App() {
   return (
-    <div className="App">
-      <span>app</span>
+    <div className="app">
+      <main className="app-main">
+        <p>country quiz</p>
+        <div className="card-with-padding">
+          <img src={TopIcon} alt="top-icon" />
+        </div>
+      </main>
+
+      <footer className="app-footer">
+        created by<p>&nbsp;oliveiraD4vi&nbsp;</p>- devChallenges.io
+      </footer>
     </div>
   );
 }
